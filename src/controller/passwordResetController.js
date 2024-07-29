5017344e2cf0d6bdd51fdf6544f9888feec7ad74
Victor Molina <molinavitillo@gmail.com>
2024-07-29 13:16:23 -0300

    desafio documentacion

--- src/controller/passwordResetController.js
+++ src/controller/passwordResetController.js
@@ -86,12 +86,15 @@ export const updatePassword = async (req, res) => {
 
     const isSame = validatePassword(newPassword, user);
     if (isSame) {
-      return CustomError.createError(
-        "ERROR",
-        null,
-        "New password cannot be the same as the old password",
-        ERROR_TYPES.INVALID_ARGUMENTS
-      );
+      // return CustomError.createError(
+      //   "ERROR",
+      //   null,
+      //   "New password cannot be the same as the old password",
+      //   ERROR_TYPES.INVALID_ARGUMENTS
+      // );
+      return res
+        .status(400)
+        .send("New password cannot be the same as the old password");
     }
 
     const hash = createHash(newPassword);
@@ -103,7 +106,7 @@ export const updatePassword = async (req, res) => {
     return CustomError.createError(
       "ERROR",
       null,
-      "No user with that email",
+      `No user with that email, ${err.messag}`,
       ERROR_TYPES.INTERNAL_SERVER_ERROR
     );
   }
